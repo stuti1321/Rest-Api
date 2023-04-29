eb5b31b3ae9eac3c864e60e57012bfac17d31a1c
stuti1321 <98119117+stuti1321@users.noreply.github.com>
2023-04-29 12:30:41 +0530

    Create SpringBootMongoDBapplication.java

---
+++ SpringBootMongoDBapplication.java
@@ -0,0 +1,14 @@
+package com.project.springbootmongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmongoDBprojectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootmongoDBprojectApplication.class, args);
+	}
+
+}
