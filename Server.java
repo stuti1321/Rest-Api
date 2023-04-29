3ebb7ac710734383b9b10d842d4ddae6edd9489e
stuti1321 <98119117+stuti1321@users.noreply.github.com>
2023-04-29 12:27:25 +0530

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,65 @@
+package com.project.springbootmongoDB;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "cluster0")
+public class Server {
+    @Id
+    private String id;
+    private String name;
+    private String language;
+    private String framework;
+
+    // Constructors, getters and setters
+
+    public Server(String id ,String name, String language, String framework) {
+        this.id = id;
+        this.name = name;
+        this.language = language;
+        this.framework = framework;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+
+    @Override
+    @JsonCreator
+    public String toString() {
+        return "Server{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", language='" + language + '\'' +
+                ", framework='" + framework + '\'' +
+                '}';
+    }
+}
